592cc512be378c942ed5050f2ac000ca00284f96
readf0x <davis.a.forsythe@gmail.com>
2025-07-29 20:39:35 -0500

    bump version to v0.3.5.1

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
         integral = pkgs.buildGoModule rec {
           name = "integral";
           pname = name;
-          version = "v0.3.4";
+          version = "v0.3.4.1";
 
           src = ./.;
 
