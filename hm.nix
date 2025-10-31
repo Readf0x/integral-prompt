239bf57b70cf409e1cb06be418211bef69d284de
readf0x <davis.a.forsythe@gmail.com>
2025-10-30 21:56:08 -0500

    add bash support to home manager

--- hm.nix
+++ hm.nix
@@ -1,8 +1,10 @@
 self: { config, lib, pkgs, ... }: let
-  cfg = config.programs.zsh.integral-prompt;
+  cfg = config.programs.integral-prompt;
 in {
-  options.programs.zsh.integral-prompt = {
+  options.programs.integral-prompt = {
     enable = lib.mkEnableOption "integral prompt";
+    enableZshIntegration = lib.hm.shell.mkZshIntegrationOption { inherit config; };
+    enableBashIntegration = lib.hm.shell.mkBashIntegrationOption { inherit config; };
     package = lib.mkOption {
       type = lib.types.package;
       default = self.packages.${pkgs.system}.default;
@@ -32,11 +34,15 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
 
-    programs.zsh.initContent = (
+    programs.zsh.initContent = lib.mkIf cfg.enableZshIntegration (
       lib.mkOrder 600 ''
         eval "$(${lib.getExe cfg.package} init zsh)"
       ''
     );
+
+    programs.bash.initExtra = lib.mkIf cfg.enableBashIntegration ''
+      eval "$(${lib.getExe cfg.package} init bash)"
+    '';
 
     home.file.${cfg.configPath}.text = builtins.toJSON ({
       "$schema" = "${self.packages.${pkgs.system}.default}/share/integral/schema.json";
