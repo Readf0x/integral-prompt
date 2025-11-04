4645c4f938c33325e954b56acf89b73ccf130f75
readf0x <davis.a.forsythe@gmail.com>
2025-11-04 15:20:42 -0600

    improve fish support

--- share/integral/init.fish
+++ share/integral/init.fish
@@ -1,15 +1,21 @@
 function fish_prompt
+	set sig $status
 	if set -q int_transient
 		echo -ne '\r\e[J'
 		integral transient
 		set -e int_transient
 	else
-		integral render fish $COLUMNS $status $(jobs | wc -l) | source
+		integral render fish $COLUMNS $sig $(jobs | wc -l) | source
 	end
 end
 function fish_right_prompt
 	echo "$integral_right_prompt_string"
 end
+
+function __int_resize --on-signal WINCH
+	commandline -f repaint
+end
+
 function __int_transient_execute
 	commandline -f expand-abbr suppress-autosuggestion
 	if commandline --is-valid || test -z "$(commandline)"
