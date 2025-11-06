cd6c5c066ace89c29f676ad9e41b70839a1316eb
readf0x <davis.a.forsythe@gmail.com>
2025-11-05 20:26:17 -0600

    refactor sshplus
    removed duplicated and inefficient logic

--- modules/sshplus.go
+++ modules/sshplus.go
@@ -3,71 +3,47 @@ package modules
 import (
 	"fmt"
 	"integral/config"
-	"os"
 )
 
 type SshPlus struct {
-	User   string
-	Host   string
-	Distro string
+	Ssh    SshModule
+	HasSsh bool
+	Db     DistroboxModule
+	HasDb  bool
 }
 
 func (m *SshPlus) Initialize(cfg *config.PromptConfig) bool {
-	if os.Getenv("SSH_CONNECTION") != "" {
-		var err error
-		m.User = os.Getenv("USER")
-		m.Host, err = os.Hostname()
-		if err != nil {
-			m.Host = os.Getenv("HOSTNAME")
-		}
-	}
-	m.Distro, _ = os.LookupEnv("CONTAINER_ID")
-	if m.User != "" || m.Distro != "" {
-		return true
-	}
-	return false
+	m.HasSsh = m.Ssh.Initialize(cfg)
+	m.HasDb = m.Db.Initialize(cfg)
+	return m.HasSsh || m.HasDb
 }
 func (m *SshPlus) Render(cfg *config.PromptConfig) RenderedModule {
-	var ssh, db, final string
-	var ln int
-	if m.User != "" {
-		var user, at, host string
-		if cfg.Ssh.User.Visible {
-			ln += len(m.User)
-			user = Sh.Fg(m.User, cfg.Ssh.User.Color)
-		}
-		if cfg.Ssh.At.Visible {
-			ln += 1
-			at = Sh.Fg("@", cfg.Ssh.At.Color)
-		}
-		if cfg.Ssh.Host.Visible {
-			ln += len(m.Host)
-			host = Sh.Fg(m.Host, cfg.Ssh.Host.Color)
-		}
-		ssh = fmt.Sprint(user, at, host)
+	var state uint8 = 0
+	if m.HasSsh {
+		state |= 1
 	}
-	if m.Distro != "" {
-		ln += len(m.Distro)
-		color, icon := cfg.Distrobox.DefaultIcon.Color, cfg.Distrobox.DefaultIcon.Icon
-		for _, distro := range *cfg.Distrobox.IconEntries {
-			if m.Distro == distro.Name {
-				color, icon = distro.Color, distro.Icon
-			}
+	if m.HasDb {
+		state |= 1 << 1
+	}
+	switch state {
+	case 0b01:
+		return m.Ssh.Render(cfg)
+	case 0b10:
+		return m.Db.Render(cfg)
+	case 0b11:
+		ssh := m.Ssh.Render(cfg)
+		db := m.Db.Render(cfg)
+		return RenderedModule{
+			Length: ssh.Length + db.Length + 2,
+			Fmt: fmt.Sprint(
+				ssh.Fmt,
+				Sh.Fg("[", cfg.Ssh.At.Color),
+				db.Fmt,
+				Sh.Fg("]", cfg.Ssh.At.Color),
+			),
+			Wrap: false,
 		}
-		db = fmt.Sprint(Sh.Fg(m.Distro, cfg.Distrobox.TextColor), Sh.Fg(string(icon), color))
 	}
-	if ssh != "" && db != "" {
-		final = ssh + Sh.Fg("[", cfg.Ssh.At.Color) + db + Sh.Fg("]", cfg.Ssh.At.Color)
-		ln += 2
-	} else if ssh != "" {
-		final = ssh
-	} else if db != "" {
-		final = db
-	}
-	return RenderedModule{
-		Length: ln,
-		Fmt:    final,
-		Wrap:   false,
-	}
+	return RenderedModule{}
 }
 
