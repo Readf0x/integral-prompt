b00273725b06d1ebd985969e7f61478956e5014b
readf0x <readf0x@duck.com>
2026-03-05 14:56:59 -0600

    fixed weird wrapping behavior

--- module_loader.zsh
+++ module_loader.zsh
@@ -10,22 +10,22 @@ integral:module:git() {
       format_str="%{%F{11}%}$branch⎇"
       if ! git diff --quiet --ignore-submodules 1>/dev/null 2>&1 || [[ $(git ls-files -o --exclude-standard) ]]; then
         local num=$(($(git ls-files -o --exclude-standard | wc -l) + $(git diff --name-only | wc -l)))
-        length=$(($length + $num + 2))
+        length=$(($length + ${#num} + 2))
         format_str="$format_str %{%F{9}%}$num✘"
       fi
       if ! git diff --quiet --ignore-submodules --cached 1>/dev/null 2>&1; then
         local num=$(git diff --ignore-submodules --cached --name-only | wc -l)
-        length=$(($length + $num + 2))
+        length=$(($length + ${#num} + 2))
         format_str="$format_str %{%F{11}%}$num+"
       fi
       if [[ $(git remote) ]] && git cherry >/dev/null 2>&1 && [[ $(git cherry | wc -l) -gt 0 ]]; then
         local num=$(git cherry | wc -l)
-        length=$(($length + $num + 2))
+        length=$(($length + ${#num} + 2))
         format_str="$format_str %{%F{14}%}$num↑"
       fi
       if [[ $(git rev-list origin/${branch} --not HEAD 2>/dev/null) ]]; then
         local num=$(git rev-list origin/${branch} --not HEAD --count)
-        length=$(($length + $num + 2))
+        length=$(($length + ${#num} + 2))
         format_str="$format_str %{%F{14}%}$num↓"
       fi
     fi
@@ -116,36 +116,59 @@ export integral_modules=(
 )
 
 # BUG: leaves <space> at end of prompt
-# BUG: Does not count for 
 integral:loop_modules() {
-  export -i position=0
-  export -i max_len=$(($COLUMNS - 1))
+  local -i position=0
+  if [[ $2 ]]; then
+    local -i max_len=$2
+  else
+    local -i max_len=$(($COLUMNS - 1))
+  fi
+
+  [[ $1 ]] && print -P "%{%F{14}%}\$max_len: %{%F{13}%}$max_len"
 
   local newline=$'\n'
   PROMPT="$newline$integral_top"
 
   for module in $integral_modules; do
-    local length=$(integral:module:$module 1)
+    [[ $1 ]] && print -P "%{%F{12}%}current module: %{%F{11}%}$module%{%F{15}%}"
+    local -i length=$(integral:module:$module 1)
     local format_str=$(integral:module:$module)
 
+    [[ $1 == "1" ]] && print "$length:$format_str"
+    [[ $1 == "2" ]] && print -P "$length:$format_str%{%F{15}%}%}"
+
     if [[ $length -gt 0 ]]; then
-      local new_pos=$(($position + $length))
+      local new_pos=$(($position + $length + 1))
+      [[ $1 ]] && print -P "%{%F{14}%}\$new_pos: %{%F{13}%}$new_pos"
       if [[ $length -gt $max_len ]] && ! integral:module:$module w; then
+        [[ $1 ]] && print -P "%{%F{14}%}length > max_len: %{%F{13}%}$length > $max_len"
         local -i i=0
         while [[ $i -le $(($length / $max_len)) ]]; do
           PROMPT+="$newline$integral_mid${format_str:$(($i * $max_len)):$max_len}"
+          [[ $1 == "1" ]] && print "${(%):-%F{15}PROMPT: $PROMPT" #}" #idk why but treesitter is freaking out over this
+          [[ $1 == "2" ]] && print -P $PROMPT
           i+=1
         done
         position=$(($length % $max_len))
+        [[ $1 ]] && print -P "%{%F{14}%}position changed to: %{%F{13}%}$position"
       elif [[ $new_pos -gt $max_len ]]; then
+        [[ $1 ]] && print -P "%{%F{14}%}new_pos > max_len: %{%F{13}%}$new_pos > $max_len"
         PROMPT+="$newline$integral_mid$format_str "
-        position=0
+        [[ $1 == "1" ]] && print "${(%):-%F{15}PROMPT: $PROMPT" #}" #idk why but treesitter is freaking out over this
+        [[ $1 == "2" ]] && print -P $PROMPT
+        position=$length
+        [[ $1 ]] && print -P "%{%F{14}%}position changed to: %{%F{13}%}$position"
       else
         PROMPT+="$format_str "
+        [[ $1 == "1" ]] && print "${(%):-%F{15}PROMPT: $PROMPT" #}" #idk why but treesitter is freaking out over this
+        [[ $1 == "2" ]] && print -P $PROMPT
         position=$new_pos
       fi
+    elif [[ $1 ]]; then
+      print -P "%{%F{14}%}length is 0, skipping...$newline"
     fi
   done
   PROMPT+="$newline%{%F{11}%}$integral_bot%{%F{15}%}"
+  [[ $1 ]] && print -P "%{%F{15}%}%====="
 }
 
