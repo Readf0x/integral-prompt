fffe8501e91a45d5af1cf45f6f224813bb4f86e9
readf0x <readf0x@duck.com>
2026-03-05 14:56:56 -0600

    dev shell working

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    devShells."x86_64-linux".default = import ./shell.nix { inherit pkgs; };
+  outputs = { self, nixpkgs, ... }@inputs: 
+  {
+    devShells."x86_64-linux".default = import ./shell.nix {
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    };
   };
 }
