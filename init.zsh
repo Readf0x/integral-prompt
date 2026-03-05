d5d3bf531768d2b1dd974956027f3673f157a62b
readf0x <readf0x@duck.com>
2026-03-05 14:56:58 -0600

    prompt now uses module system

--- init.zsh
+++ init.zsh
@@ -15,6 +15,9 @@ export integral_vim_indicators=(
   "◐" # v-line
   "●" # normal
 )
+export integral_top="⌠"
+export integral_mid="⎮"
+export integral_bot="⌡"
 
 # === CONFIG LOADING ===
 local rc_locations=(
@@ -40,11 +43,6 @@ integral:helpers:cursor-shape() {
     echo -ne '\e[5 q'
   fi
 }
-integral:helpers:real-length() {
-  local x=$(print $1 | sed 's/%{\(%F\|%B\)\{0,2\}{[0-9]*}%}//g')
-  export debug_len=$x
-  print ${#x}
-}
 integral:helpers:version() {
   print "v$version"
 }
@@ -52,74 +50,7 @@ integral:helpers:version() {
 
 # === MAIN LOGIC ===
 integral:prompt() {
-  # Variables
-  case $VI_KEYMAP in
-    INSERT)
-      visym="%{%F{10}%}${integral_vim_indicators[1]}"
-      ;;
-    VISUAL)
-      visym="%{%F{13}%}${integral_vim_indicators[2]}"
-      ;;
-    V-LINE)
-      visym="%{%F{13}%}${integral_vim_indicators[3]}"
-      ;;
-    NORMAL)
-      visym="%{%F{9}%}${integral_vim_indicators[4]}"
-      ;;
-  esac
-  local dir=${PWD/$HOME/\~}
-  local git
-
-  # Constants
-  local newline=$'\n'
-  local prompt_top="$newline%{%F{11}%}⌠$visym%(?.. %{%F{9}%}⚠)"
-  local prompt_bot="$newline%{%F{11}%}⌡%{%F{255}%}"
-
-  if ! $(git rev-parse --is-bare-repository 2>/dev/null); then
-    if [ -d .git ] || git rev-parse --git-dir >/dev/null 2>&1; then
-      git="%{%F{11}%}$(git rev-parse --abbrev-ref HEAD 2>/dev/null)⎇"
-      if ! git diff --quiet --ignore-submodules 1>/dev/null 2>&1 || [[ $(git ls-files -o --exclude-standard) ]]; then
-        git="$git %{%F{9}%}$(($(git ls-files -o --exclude-standard | wc -l) + $(git diff --name-only | wc -l)))✘"
-      fi
-      if ! git diff --quiet --ignore-submodules --cached 1>/dev/null 2>&1; then
-        git="$git %{%F{11}%}$(git diff --ignore-submodules --cached --name-only | wc -l)+"
-      fi
-      if [[ $(git remote) ]] && git cherry >/dev/null 2>&1 && [[ $(git cherry | wc -l) -gt 0 ]]; then
-        git="$git %{%F{14}%}$(git cherry | wc -l)↑"
-      fi
-      if [[ $(git rev-list FETCH_HEAD --not HEAD 2>/dev/null) ]]; then
-        git="$git %{%F{14}%}$(git rev-list FETCH_HEAD --not HEAD --count)↓"
-      fi
-    fi
-  else git="%{%F{11}%}bare⎇"; fi
-  # BUG: this will not work if other modules push the prompt past the terminal width
-  # TODO: Change the loop method to be more efficient
-  #   Instead of whatever the hell is happening here, we can create static module
-  #   "classes" (just a function that follows a certain schema in this case) that output
-  #   a preformatted string as well as their raw length. Then we can loop through all
-  #   the modules and add their length to the current length of the line. If it exceeds
-  #   the terminal width, we just insert our newline and set the length back to 0.
-  if (( ${#dir} >= $COLUMNS )); then
-    # This is where stuff gets messy, shell string concatenation is less than readable.
-    # Set the first line, must be done seperately because of the indicator
-    prompt_top="$prompt_top %{%F{12}%}${dir:0:$(($COLUMNS - 3))}"
-    # Determine the number of lines the rest of the directory path will take up
-    local x=$((${#dir} / $COLUMNS + 1))
-    for ((i = 1; i < x; i++)); do
-      # Append the correct section of the path by offsetting the substring based on $i
-      prompt_top="$prompt_top$newline%{%F{11}%}⎮%{%F{12}%}${dir:$((($COLUMNS - 1) * $i - 3)):$((($COLUMNS - 1)))}"
-    done
-    # TODO: detect if the last line is short enough to not need a new line and if the
-    # rest of the "modules" need to go through a similar process to the directory path.
-    if [[ $git ]]; then
-      prompt_top="$prompt_top$newline%{%F{11}%}⎮%{%F{12}%}$git"
-    fi
-  else
-    # real simple if the directory path is short enough
-    prompt_top="$prompt_top %{%F{12}%}$dir $git"
-  fi
-
-  PROMPT="$prompt_top$prompt_bot"
+  integral:loop_modules
 }
 
 # === ZLE ===
