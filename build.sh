37818af3ff776cbf23388637bf759bf0dd8d4809
readf0x <readf0x@duck.com>
2026-03-05 14:57:13 -0600

    add debian build

--- build.sh
+++ build.sh
@@ -6,5 +6,28 @@ go generate
 go build
 cp integral build/bin
 cp -r share build
-tar -cJf build.tar.xz -C build .
+# package step
+case "$1" in
+  deb)
+    TAG="$(git describe --tags --abbrev=0)"
+    mkdir build/DEBIAN
+    cat > build/DEBIAN/control <<EOF
+Package: integral-prompt
+Version: ${TAG#v}
+Section: utils
+Priority: optional
+Architecture: amd64
+Maintainer: Jean <https://github.com/readf0x>
+Description: Math themed shell prompt
+EOF
+    mkdir -p build/usr/local/bin
+    chmod 755 build/bin/*
+    mv build/bin/* build/usr/local/bin/
+    rmdir build/bin
+    dpkg-deb --build build
+  ;;
+  *)
+    tar -cJf build.tar.xz -C build .
+  ;;
+esac
 
